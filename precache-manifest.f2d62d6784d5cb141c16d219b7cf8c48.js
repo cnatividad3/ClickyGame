f01131257f9fcd91738d7ad091f4c275808f8dda
Chris Natividad <cnat@Chriss-MacBook-Pro.local>
2018-11-13 23:28:52 -0500

    Updates

---
+++ precache-manifest.f2d62d6784d5cb141c16d219b7cf8c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1eb84f96895179cd544",
+    "url": "/static/js/main.d1eb84f9.chunk.js"
+  },
+  {
+    "revision": "14622a5fd2fc5607088a",
+    "url": "/static/js/1.14622a5f.chunk.js"
+  },
+  {
+    "revision": "d1eb84f96895179cd544",
+    "url": "/static/css/main.f3487188.chunk.css"
+  },
+  {
+    "revision": "300fe09d04b7bfb83229f58332580523",
+    "url": "/index.html"
+  }
+];
